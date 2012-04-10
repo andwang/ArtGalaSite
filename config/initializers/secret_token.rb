5b24049f2ee326056202204ebd3d7c58fca29f01
Ludo Antonov <ludo@hulu.com>
2012-04-09 23:54:03 -0700

    +Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artfire::Application.config.secret_token = 'bcc51af75574c6a0748322837dba64b3f3e6ab17c7b776596115f669a84a7222ae110d7f7620c5b2e46eb48386cde3a165d7802161b68aa217be1e1bd359a52a'
