1d8084318d29af65adb6a4d63c550eebd0bf5fc3
Ludo Antonov <ludo@hulu.com>
2012-04-11 04:38:10 -0700

    Working locally

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
    def index
-       format.html
+       respond_to do |format|
+           format.html
+       end
        #render :json => { :ok => 1}
    end 
 end
